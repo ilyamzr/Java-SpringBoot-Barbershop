aecd98ddde7b73fd61e97f929e00f6e36076f93a
Ilya <35830175@study.bsuir.by>
2025-02-24 21:53:26 +0300

    .

--- src/main/java/com/example/Barbershop/BarbershopApplication.java
+++ src/main/java/com/example/Barbershop/BarbershopApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.barbershop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
